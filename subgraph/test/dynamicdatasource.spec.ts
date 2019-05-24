2978c1b58927e4d83c06ba9d0f9ab041198bbcf0
dOrgJelli <jelli@dorg.tech>
2019-05-24 01:21:20 -0400

    add jest

---
+++ subgraph/test/dynamicdatasource.spec.ts
@@ -0,0 +1,13 @@
+const Avatar = require("dynamic-datasource-test-protocol/build/contracts/Avatar.json");
+
+describe("Dynamic Data Source Test", () => {
+  /*beforeAll(async () => {
+    // deploy dao
+  });*/
+
+  it("Avatar.setName Updates The Graph", async () => {
+    // query to verify the old name
+    // set name
+    // query to verify the new name
+  });
+});
